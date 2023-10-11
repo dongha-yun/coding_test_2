5534a8d1cdf3fc9dff73b33941c2fe919df1478d
Yun, Dong-Ha <dongha.yun@kakao.com>
2023-10-11 10:35:49 +0900

    no message

--- codingTest/src/coding_test_lecture/chap01/Test_01_03.java
+++ codingTest/src/coding_test_lecture/chap01/Test_01_03.java
@@ -1,4 +1,36 @@
 package coding_test_lecture.chap01;
 
+import java.util.Scanner;
+
 public class Test_01_03 {
+  public String solution(String str) {
+    String answer = "";
+
+    //////// Integer.MIN_VALUE : 처음에는 가장 작은 값으로 초기화
+    int m = Integer.MIN_VALUE;
+
+    //////// split 으로 String 배열 처리
+    String[] s = str.split(" ");
+
+    for (String x : s) {
+//      System.out.println(x);
+      int len = x.length();
+      if (len > m) {
+        m = len;
+        answer = x;
+      }
+    }
+
+    return answer;
+  }
+
+  public static void main(String[] args) {
+    Test_01_03 T = new Test_01_03();
+    Scanner kb = new Scanner(System.in);
+
+    //////// .nextList() : 문장으로 입력받아야 하므로
+    String str = kb.nextLine();
+
+    System.out.println(T.solution(str));
+  }
 }
