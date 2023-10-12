775afe06aa2aa1cd0480e0afe8ef576406a5968c
Yun, Dong-Ha <dongha.yun@kakao.com>
2023-10-12 09:09:10 +0900

    no message

--- codingTest/src/coding_test_lecture/chap01/Test_01_05.java
+++ codingTest/src/coding_test_lecture/chap01/Test_01_05.java
@@ -1,4 +1,17 @@
 package coding_test_lecture.chap01;
 
+import java.util.Scanner;
+
 public class Test_01_05 {
+  public String solution(String str) {
+    String answer = "";
+    return answer;
+  }
+
+  public static void main(String[] args) {
+    Test_01_05 T = new Test_01_05();
+    Scanner kb = new Scanner(System.in);
+    String str = kb.nextLine();
+    System.out.println(T.solution(str));
+  }
 }
